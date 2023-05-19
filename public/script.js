4307802582b648907007026ec5001df9909b28db
akum7246 <ashray9797@gmail.com>
2023-05-19 23:58:43 +1000

    static values replaced in object definition

--- public/script.js
+++ public/script.js
@@ -1,19 +1,41 @@
+// let task = {
+
+//     name :"Park Run",
+//     category :"Long Distance",
+//     // used this id property to create unique id everytime
+//     id : Date.now(),
+//     dateOfRun:new Date ().toISOString(),
+//     duration: 1,
+//     distance:10,
+//     pace:7,
+//     exertion:4,
+//     terrain:"Road",
+//     timeOfDay:"morning",
+//     description:"This run was especially hard because it rained...",
+//     distanceMetric:"Kilometers",
+//     image:"longDistance.jpg",
+// }
+
+var taskList=[];
+// created function called taskList that has all the inputparameters to create task object
+function addTask(name, category, dateOfRun, 
+duration, distance, exertion, terrain,
+timeOfDay, description, distanceMetric){
 let task = {
-    name :"Park Run",
-    category :"Long Distance",
+    name,
+    category,
     // used this id property to create unique id everytime
     id : Date.now(),
     dateOfRun:new Date ().toISOString(),
-    duration: 1,
-    distance:10,
+    duration,
+    distance,
     pace:7,
-    exertion:4,
-    terrain:"Road",
-    timeOfDay:"morning",
-    description:"This run was especially hard because it rained...",
-    distanceMetric:"Kilometers",
+    exertion,
+    terrain,
+    timeOfDay,
+    description,
+    distanceMetric,
     image:"longDistance.jpg",
 }
-
-console.log(task);
+}
 
